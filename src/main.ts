2b6fa0bbbc7c2b3d41cbc094d0eae64bde0bbe71
sylvainSUPINTERNET <sylvain.joly@supinternet.fr>
2023-06-18 23:19:49 +0200

    [TO FIX]

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,10 @@
 import { Browser, chromium, ChromiumBrowser, LaunchOptions, Page } from 'playwright';
 
+
+
 ( async () => {
+
+    const deliveryCity = "92130 - ISSY LES MOULINEAUX";
 
     const today = new Date();
     const currentWeekday = today.getDay(); // Sunday: 0, Monday: 1, ..., Friday: 5
@@ -41,6 +45,8 @@ import { Browser, chromium, ChromiumBrowser, LaunchOptions, Page } from 'playwri
     // _1towkzva flexCenter _1towkzvd
     const datesMenu = await page.$$('.root-0-2-156');
 
+    
+
     for ( const element of datesMenu ) {
         let textEl = await element.innerText(); 
         if ( textEl != undefined ) {
@@ -53,8 +59,77 @@ import { Browser, chromium, ChromiumBrowser, LaunchOptions, Page } from 'playwri
 
     }
 
+    // TODO : out of stock ( must ignore ) 
+    // Probleme quand on rajoute ça rajoute pas ( quand on change de date ) 
 
-    const clickedFirst = false;
+    await page.waitForTimeout(10000);
+
+    // Scroll down the page
+    await page.evaluate(() => {
+        window.scrollBy(0, window.innerHeight);
+        });
+    
+    // Wait for some time to let the page finish scrolling
+    await page.waitForTimeout(2000); // Adjust the timeout value as needed
+      
+
+
+    let clickedFirst = false;
+
+    const buttonAddMeal = await page.$$('._1towkzva'); // Use the $$ method to select elements
+
+    for (const btn of buttonAddMeal) {
+    //   const textContent = await btn.evaluate((b) => b.textContent);
+      
+        if ( !clickedFirst ) {
+
+            await btn.click(); // will open city since it's first time
+
+            console.log("First item fill city info...")
+            // Validate the city
+            
+            await page.waitForSelector('input[name="zipCode"]');
+            await page.type('input[name="zipCode"]', deliveryCity);
+
+            const btnValidateCity = await page.$$('#valider');
+            await btnValidateCity[0].click();
+
+            console.log("City info filled and validate")
+
+
+            await page.waitForTimeout(5000);
+            
+            console.log("selecting regime ...")
+            const changeNbOfMealsBtn = await page.$$('.price-0-2-570');
+            await changeNbOfMealsBtn[0].click();
+
+            await page.waitForTimeout(1500);
+
+            console.log("choosing price");
+            const meals14 = await page.$$('.planSelected14');
+            console.log("meals14", meals14.length);
+            await meals14[0].click();
+            await page.waitForTimeout(1000);
+            const mealRegimValidateBtn = await page.$$('#choisir_la_formule_14_plats')
+            await mealRegimValidateBtn[0].click();
+
+            await page.waitForTimeout(4000);
+
+            clickedFirst = true;
+            // select first item
+
+            await btn.click();
+
+            
+
+        }
+      
+        console.log("Not first btn ...")
+        await page.waitForTimeout(4000);
+
+        await btn.click();
+    }
+
 
     
 
